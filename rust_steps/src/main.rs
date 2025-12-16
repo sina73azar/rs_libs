d425fdf696f1f5707d3dd9e2c84cd5c1793de4c4
“Mr.C” <s.azar.engr@gmail.com>
2025-12-16 10:42:03 +0330

    feat: Add initial Rust project `rust_steps`
    This commit introduces a new Rust project named `rust_steps`.

---
+++ rust_steps/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    println!("Hello, world!");
+        let x = 10;        // immutable
+        let mut y = 10;    // mutable
+        y = y + 5;
+
+        let z = {
+            let a = 2;
+            a * 3           // no semicolon => expression value
+        };
+
+        println!("x={x}, y={y}, z={z}");
+}
